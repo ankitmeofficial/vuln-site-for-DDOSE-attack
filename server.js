abbc5f7cb28dbbb87d350b560d9067f8972a752b
ankitmeofficial <ankitmeofficial@gmail.com>
2025-12-17 10:49:34 +0530

    updated files

--- server.js
+++ server.js
@@ -3,6 +3,33 @@ const axios = require("axios");
 
 const app = express();
 
+// Express.js Middleware
+// Add this to your Express app (usually in app.js or server.js)
+
+app.use((req, res, next) => {
+  // Send tracking request asynchronously (non-blocking)
+  fetch("http://localhost:5000/api/track", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-website-key": "TrJTkBIpvFGV1ANS3YRqIsaJ"
+    },
+    body: JSON.stringify({
+      ip: req.ip || req.connection.remoteAddress || req.headers['x-forwarded-for']?.split(',')[0] || 'unknown',
+      route: req.originalUrl || req.url,
+      method: req.method,
+      userAgent: req.headers["user-agent"] || ""
+    })
+  }).catch(() => {
+    // Silently fail - don't block user's request
+  });
+  next();
+});
+
+
+
+
+
 app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "ejs");
@@ -39,3 +66,13 @@ app.get("/news/:id", async (req, res) => {
 app.listen(4000, () =>
   console.log("Vulnerable News website running at http://localhost:4000")
 );
+
+
+
+
+
+
+
+
+
+
